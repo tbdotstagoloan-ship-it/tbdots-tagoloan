3ac2de075dd210248bc03399974dc3cfcf17164a
tbdotstagoloan-ship-it <tbdotstagoloan@gmail.com>
2025-10-03 19:05:16 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
     <div class="login-container">
         <div class="login-header">
             <h2>Welcome to TB DOTS [PMS]</h2>
-            <h1 style="color:red;">Version 3.0</h1>
+            <h1 style="color:red;">Version 3.1</h1>
         </div>
 
         <!-- Laravel Status Message -->
